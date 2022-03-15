68ad329fc8b97c971bfb8533979ae91c3cb05819
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2022-03-15 17:18:54 -0400

    update to tiny_gltf v2.5.0

--- ext/tiny_gltf/rb_tiny_gltf_sampler.cpp
+++ ext/tiny_gltf/rb_tiny_gltf_sampler.cpp
@@ -10,7 +10,9 @@ VALUE rSampler_new(const Sampler *sampler, VALUE rmodel) {
   rb_ivar_set(rsampler, rb_intern("@mag_filter"), texture_filter_to_sym(sampler->magFilter));
   rb_ivar_set(rsampler, rb_intern("@wrap_s"),     texture_wrap_to_sym(sampler->wrapS));
   rb_ivar_set(rsampler, rb_intern("@wrap_t"),     texture_wrap_to_sym(sampler->wrapT));
-  rb_ivar_set(rsampler, rb_intern("@wrap_r"),     texture_wrap_to_sym(sampler->wrapR));
+  // removed from tiny_gltf until a spec or extension actually calls for it
+  // https://github.com/syoyo/tinygltf/issues/287
+  // rb_ivar_set(rsampler, rb_intern("@wrap_r"),     texture_wrap_to_sym(sampler->wrapR));
   rb_ivar_set(rsampler, rb_intern("@extras"),     rValue_new(&sampler->extras, rmodel));
 
   return rsampler;
